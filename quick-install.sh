837eaa2deaade1db46d935e4b0fffe0360a05a23
dhsimpson <qeadzc9503@naver.com>
2022-03-27 21:22:16 +0900

    Update quick-install.sh

--- quick-install.sh
+++ quick-install.sh
@@ -30,12 +30,12 @@ sudo mkdir /etc/docker
 
 cat <<EOF | sudo tee /etc/docker/daemon.json
 {
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
+"exec-opts": ["native.cgroupdriver=systemd"],
+"log-driver": "json-file",
+"log-opts": {
+"max-size": "100m"
+},
+"storage-driver": "overlay2"
 }
 EOF 
 
